7c93df2a94c75c558b55d65a2d547312f3521d5d
AlejandroLHC <alejandrolhc@gmail.com>
2022-08-10 17:56:54 +0200

    Add info user

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,12 +1,56 @@
-let 
+const //Posts data
     titles = [],
     bodies = [],
     IDs = [],
-    userIDs = [];
+    userIDsPost = [];
 
+const //Users data
+    userId = [],
+    userName = [],
+    userUsername = [],
+    userEmail = [],
+    userAddress = [],
+    userPhone = [],
+    userWebsite = [],
+    userCompany = [];
+
+// Comments Data
+const comments = []
+    
+window.onload = storeUsers();
+
+function storeUsers() {
+    fetch("http://localhost:3000/users")
+        .then(res => res.json())
+        .then(data => {
+            console.log(data);
+            data.forEach(e => {
+                userId.push(e.id)
+                userName.push(e.name)
+                userUsername.push(e.username)
+                userEmail.push(e.email)
+                userAddress.push(e.address)
+                userPhone.push(e.phone)
+                userWebsite.push(e.website)
+                userCompany.push(e.company)
+            });
+        });
+}
+
+window.onload = storeComments();
+
+function storeComments() {
+    fetch("http://localhost:3000/users")
+        .then(res => res.json())
+        .then(data => {
+            console.log(data);
+            data.forEach(e => {
+                comments.push(e)
+            });
+        });
+}
 
 window.onload = displayPosts();
-
 
 function displayPosts() {
     while (document.getElementById('postsContainer').firstChild) {
@@ -20,7 +64,7 @@ function displayPosts() {
                 titles.push(e.title);
                 bodies.push(e.body);
                 IDs.push(e.id);
-                userIDs.push(e.userId);
+                userIDsPost.push(e.userId);
             });
             for (let i = titles.length - 1; i >= 0; i--) {
                 let article = document.createElement('article');
@@ -29,8 +73,8 @@ function displayPosts() {
                 article.innerHTML = `
                 <div class="row title-container">
                 <div class="col-2"></div>
-                <h4 id="tit${i}" class="col-8 post-title">${titles[i]}</h4>
-                <button onclick="editPost('tit${i}', 'cont${i}', '${IDs[i]}')" class="col-1 edit-btn"><i class="fa-solid fa-pen-to-square"></i></button>
+                <h4 onclick="moreInfo('${IDs[i]}', '${userIDsPost[i]}')" id="tit${i}" class="col-8 post-title">${titles[i]}</h4>
+                <button onclick="editPost('tit${i}', 'cont${i}', '${IDs[i]}', '${userIDsPost[i]}')" class="col-1 edit-btn"><i class="fa-solid fa-pen-to-square"></i></button>
                 <button onclick="deletePost('${IDs[i]}')" class="col-1 delete-btn"><i class="fa-solid fa-trash-can"></i></button>
                 </div>
                 <p id="cont${i}" class="post-content">${bodies[i]}</p>
@@ -40,15 +84,70 @@ function displayPosts() {
     });
 }
 
+function moreInfo(id, userID) {
+    let userIndex = userId.indexOf(parseInt(userID));
 
-function editPost(titleID, contentID, id) {
+    const infoContainer = document.createElement('section');
+    infoContainer.className = "more-info-container";
+    infoContainer.setAttribute("id", "infoContainer");
+    infoContainer.innerHTML = `
+        <div class="row justify-content-center">
+            <h2 class="col-8 title-more-info">Post information</h2>
+        </div>
+        <div class="accordion" id="accordionExample">
+            <div class="accordion-item">
+                <h2 class="accordion-header" id="headingOne">
+                <button class="accordion-button" type="button" data-bs-toggle="collapse" data-bs-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
+                    <span>Author information: <b>${userUsername[userIndex]}</b></span>
+                </button>
+                </h2>
+                <div id="collapseOne" class="accordion-collapse collapse show" aria-labelledby="headingOne" data-bs-parent="#accordionExample">
+                <div class="accordion-body">
+                    <p>
+                        ${userUsername[userIndex]}'s name is: <b>${userName[userIndex]}</b>. 
+                    </p>
+                    <p>
+                        ${userName[userIndex]} works at <b>${userCompany[userIndex].name}</b>, a company located in <b>${userAddress[userIndex].city}.</b> 
+                    </p>
+                    <p>
+                        Having any questions, you may contact ${userName[userIndex]} via email: <u>${userEmail[userIndex]}</u>
+                    </p>
+                </div>
+                </div>
+            </div>
+            <div class="accordion-item">
+                <h2 class="accordion-header" id="headingTwo">
+                <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">
+                    Post information:
+                </button>
+                </h2>
+                <div id="collapseTwo" class="accordion-collapse collapse" aria-labelledby="headingTwo" data-bs-parent="#accordionExample">
+                <div class="accordion-body">
+                    <p>
+                    </p>
+                </div>
+                </div>
+            </div>
+        </div>
+        <div class="row justify-content-evenly">
+            <button onclick="closeMoreInfo();" type="button" class="col-6 btn btn-secondary">Exit</button>
+        </div>
+    `
+    if ((document.getElementById('main').firstChild.className !== "more-info-container") && 
+        (document.getElementById('main').firstChild.className !== "pop-up")) {
+        document.getElementById('main').insertAdjacentElement("afterbegin", infoContainer);
+    };
+}
+
+
+function editPost(titleID, contentID, id, userID) {
     const
     titleEle = document.getElementById(titleID),
     contentEle = document.getElementById(contentID),
     title = titleEle.textContent,
     content = contentEle.textContent,
     popUp = document.createElement('section');
-    
+
     popUp.className = "pop-up";
     popUp.setAttribute("id", "popUp");
     popUp.innerHTML = `
@@ -66,16 +165,17 @@ function editPost(titleID, contentID, id) {
             </div>
             <div class="row justify-content-evenly">
                 <button onclick="closePopUp();" type="button" class="col-3 btn btn-secondary">Cancel</button>
-                <button onclick="updatePost('${id}')" type="submit" class="col-3 btn btn-success">Save</button>
+                <button onclick="updatePost('${id}', '${userID}')" type="submit" class="col-3 btn btn-success">Save</button>
             </div>
         </form>
     `
-    if (document.getElementById('main').firstChild.className !== "pop-up") {
+    if ((document.getElementById('main').firstChild.className !== "more-info-container") && 
+        (document.getElementById('main').firstChild.className !== "pop-up")) {
         document.getElementById('main').insertAdjacentElement("afterbegin", popUp);
     }
 }
 
-function updatePost(i) {
+function updatePost(i, userID) {
     
     if (document.getElementById("form").checkValidity()) {
         const
@@ -86,7 +186,7 @@ function updatePost(i) {
             method: 'PUT',
             headers: { 'Content-Type': 'application/json' },
             body: JSON.stringify({
-                userId: i,
+                userId: userID,
                 id: i,
                 title: titleInfo,
                 body: contentInfo
@@ -95,8 +195,7 @@ function updatePost(i) {
         fetch(`http://localhost:3000/posts/${(i)}`, requestOptions)
             .then(response => response.json())
             .then(data => {
-                document.querySelector(`@${i} .post.title`).textContent = newTitle;
-                document.querySelector(`@${i} .post.content`).textContent = newContent;
+                displayPosts()
             });
     }
 }
@@ -128,7 +227,8 @@ function createPost() {
             </div>
         </form>
     `
-    if (document.getElementById('main').firstChild.className !== "pop-up") {
+    if ((document.getElementById('main').firstChild.className !== "more-info-container") && 
+        (document.getElementById('main').firstChild.className !== "pop-up")) {
         document.getElementById('main').insertAdjacentElement("afterbegin", popUp);
     }
 }
@@ -180,6 +280,9 @@ function uploadPost() {
 
 function closePopUp() {
     document.getElementById('main').removeChild(document.getElementById('popUp'));
+}
+function closeMoreInfo() {
+    document.getElementById('main').removeChild(document.getElementById('infoContainer'));
 }
 
 function deletePost(id) {
